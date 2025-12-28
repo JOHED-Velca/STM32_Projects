bd0863a1b9da358c04292b321be32fa6a1fbc75f
Jonathan Velasquez <johedvelca@gmail.com>
2025-12-27 21:06:50 -0500

    flashing Blinky_v2.1 works, LED off

---
+++ Blinky_v2/app/blinky_app.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "stm32l4xx_hal.h"
+
+typedef struct {
+	GPIO_TypeDef *port;
+	uint16_t pin;
+	uint32_t period_ms;
+	uint32_t next_toggle_ms;
+} BlinkyApp;
+
+void blinky_app_init(BlinkyApp *app, GPIO_TypeDef *port, uint16_t pin, uint32_t period_ms);
+void blinky_app_task(BlinkyApp *app, uint32_t now_ms);
---
+++ Blinky_v2/cube/cube_blinky_v2/build/Debug/CMakeFiles/3.28.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.14.0-36-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.14.0-36-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/johed/Documents/GitHub/STM32_Projects/Blinky_v2/cube/cube_blinky_v2/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
